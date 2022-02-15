6cea2aaf4f523dc20160f91c15650019dfd0ace0
BourneID7 <m.bourne2@comcast.net>
2022-02-14 18:44:30 -0600

    update packages, fix multiple authors display

--- client/src/components/BookResult.js
+++ client/src/components/BookResult.js
@@ -3,13 +3,14 @@ import React from "react";
 function BookResult(props) {
 
     const { id, title, image, authors, description, link, onClick, buttonText} = props;
+    const authorText = authors.length > 1 ? authors.join(" | ") : authors
 
     return (
 
         <article id={id}>
             {/* {image && <img src={image} alt={title} className="thumbnail" />} */}
             <h5>{title}</h5>
-            <p>By: {authors}</p>
+            <p>By: {authorText}</p>
             <p>{description}</p>
             <a href={link} target="_blank" rel="noopener noreferrer"><button className="btn waves-effect waves-light">View</button></a>
             <button 
